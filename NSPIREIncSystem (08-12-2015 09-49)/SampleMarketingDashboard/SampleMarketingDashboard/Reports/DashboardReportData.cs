f1f78fff3c05fee3a11b30e060b5a7f662deecb6
charamirez <charissej.ramirez@yahoo.com>
2015-08-18 10:59:32 +0800

    vfcbvdvp

---
+++ NSPIREIncSystem (08-12-2015 09-49)/SampleMarketingDashboard/SampleMarketingDashboard/Reports/DashboardReportData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NSPIREIncSystem.Reports
+{
+    class DashboardReportData
+    {
+        public DashboardReportData()  { }
+        public string ReportHeader { get; set; }
+        public string ReportTitle { get; set; }
+        public decimal LeadsActive { get; set; }
+        public decimal LeadsEngaged { get; set; }
+        public decimal LeadsDidNotContinue { get; set; }
+        public string OverallLeads { get; set; }
+        public List<DashboardReportDetail> details { get; set; }
+
+    }
+    class DashboardReportDetail
+    {
+        public DashboardReportDetail() { }
+       
+    }
+}
